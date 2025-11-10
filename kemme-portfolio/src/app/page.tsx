118754f3972f4ec85bbe85036335fb9d6d82c691
kemme77 <linus.kemmeter@gmx.de>
2025-11-10 14:37:30 +0100

    added first button component

--- kemme-portfolio/src/app/page.tsx
+++ kemme-portfolio/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-10">
       <h2>Portfolio von Kemme</h2>
+      <Button>Klick mich</Button>
     </div>
   );
 }
