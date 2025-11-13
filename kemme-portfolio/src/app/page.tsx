42b41d6e5d84892b1bcb046507b0faf337cae915
kemme77 <linus.kemmeter@gmx.de>
2025-11-13 09:36:42 +0100

    added spinner

--- kemme-portfolio/src/app/page.tsx
+++ kemme-portfolio/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +7,15 @@ export default function Home() {
     <div className="p-10">
       <h2>Portfolio von Kemme</h2>
       <Button>Klick mich</Button>
+      <SpinnerCustom />
     </div>
   );
+}
+
+export function SpinnerCustom() {
+  return (
+    <div className="flex items-center gap-4">
+      <Spinner className='size-120 animate-spin' />
+    </div>
+  )
 }
